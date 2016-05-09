cc2a26216de3ecec6c98ca3d6dfa45fc3046fd0b
岳时雨 <yueshiyu@qq.com>
2016-05-09 18:09:23 +0800

    version 0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # koa-canvas-captcha
 适用于Koa框架的canvas绘制的验证码库
+
+## Install 
+
+```bash
+    npm install koa-canvas-captcha
+```
+## Demo
+
+```javascript
+    var captcha = require('koa-canvas-captcha');            
+    var getCaptcha = function*(){
+         this.type = 'jpg';
+         this.set({
+             'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0',
+             'Expires': 'Sun, 12 Jan 1986 12:00:00 GMT'
+         });
+         //生成验证码 返回text和图buffer
+         var item = yield captcha({
+             length: 4, 
+             fontSize: 30, 
+             width: 150, 
+             height: 32, 
+             color: 'green', // code color,
+             background: 'rgb(245,245,245)', // captcha background color
+             lineWidth: 0.5, // Interference lines width
+             type: 'normal'
+         });
+         this.session && (this.session.captcha = item.answer);
+         this.body = item.imageBuffer
+     };
+```
+## Options
+
+* `length` - (Number )  captcha length
+* `fontSize` - (Number | default: 30 ) captcha font size
+* `width` - (Number | default: 150) image width
+* `height` - (Number | default: 32) height width
+* `lineWidth` - (Number | default: 1) background line width
+* `background` - (String | default: rgb(255,255,255)) background color
+* `color` - (String | de fault: rgb(0,0,0)) font color
+* `text` - (String) custom captcha text
+* `type` - (String | default: normal) captcha type
+   - `normal` - random letter and number 
+   - `letter` - just letter 
+   - `number` - just number 
+   - `arithmetic` - random arithmetic
+
+## Result
+  * `answer` - The correct verification code
+  * `imageBuffer` - imageBuffer
+
---
+++ index.js
@@ -0,0 +1,132 @@
+var Canvas = require('canvas');
+var Font = Canvas.Font;
+var path = require("path");
+
+if (!Font) {
+    throw new Error('Need to compile with font support');
+}
+//字体
+var enFont = new Font('enFont', path.join(__dirname, './font/captchaFont.ttf'));
+
+module.exports = function(params) {
+
+    params.color = params.color || 'rgb(0,0,0)'
+    params.background = params.background || 'rgb(255,255,255)'
+    params.lineWidth = params.lineWidth || 1
+    params.fontSize = params.fontSize || 30
+    params.codeLength = params.length || 4
+    params.canvasWidth = params.width || 150
+    params.canvasHeight = params.height || 32
+    params.type = params.type || 'normal';     //normal 普通  number 仅数字 letter 仅字母 arithmetic 算数题
+
+    return function*() {
+
+        var canvas = new Canvas(params.canvasWidth, params.canvasHeight)
+        var ctx = canvas.getContext('2d')
+
+        ctx.antialias = 'gray'
+        ctx.fillStyle = params.background
+        ctx.fillRect(0, 0, params.canvasWidth, params.canvasHeight)
+        ctx.strokeRect(0, 0, params.canvasWidth, params.canvasHeight)
+        ctx.fillStyle = params.color
+        ctx.lineWidth = params.lineWidth
+
+        ctx.addFont(enFont);
+        ctx.font = 'normal normal '+params.fontSize + 'px enFont'
+
+        ctx.strokeStyle = 'grey'
+        for (var i = 0; i < 3; i++) {
+            ctx.moveTo(10, Math.random() * params.canvasHeight)
+            ctx.bezierCurveTo(80, Math.random() * params.canvasHeight, 160, Math.random() * params.canvasHeight, params.canvasWidth - 20, Math.random() * params.canvasHeight)
+            ctx.stroke()
+        }
+
+        ctx.strokeStyle = params.color
+        var text;
+        var answer;
+        switch ( params.type ){
+            case 'number':
+                answer = text =  Math.random().toString(10).substr(2, params.codeLength).toLocaleUpperCase();
+                break;
+            case 'letter':
+                for(var i =0; i<params.codeLength; i++){
+                    text +=  String.fromCharCode(Math.ceil(Math.random() * 25) + 65).toLocaleUpperCase();
+                }
+                answer = text;
+                break;
+            case 'arithmetic':
+
+
+                switch( Math.ceil(Math.random() * 4) ){
+                    //加
+                    case 1:
+                        var num1 = Math.floor(Math.random() * 100);
+                        var num2 = Math.floor(Math.random() * 100);
+                        text =  [num1, "+", num2, "="];
+                        answer = num1 + num2;
+                        break;
+                    //减
+                    case 2:
+                        var num1 = Math.floor(Math.random() * 100);
+                        var num2 = Math.floor(Math.random() * 100);
+                        if(num1 > num2){
+                            text =  [num1, "-", num2, "="];
+                            answer = num1 - num2;
+                        }else{
+                            text =  [num2, "-", num1, "="];
+                            answer = num2 - num1;
+                        }
+                        break;
+                    //乘
+                    case 3:
+                        var num1 = Math.ceil(Math.random() * 20);
+                        var num2 = Math.ceil(Math.random() * 9);
+                        text =  [num1, "×", num2, "="];
+                        answer = num1 * num2;
+
+                        break;
+                    //除
+                    case 4:
+                        var num1 = Math.ceil(Math.random() * 9);
+                        var num2 = Math.ceil(Math.random() * 8 + 1);
+                        text =  [ num1 * num2, "÷", num1, "="];
+                        answer = num2;
+                        break;
+
+                }
+
+                params.codeLength = 4;
+                break;
+            default:
+                answer =text =  Math.random().toString(35).substr(2, params.codeLength).toLocaleUpperCase();
+        }
+
+        text = params.text || text;
+
+        for (i = 0; i < text.length; i++) {
+            ctx.setTransform(
+                Math.random() * 0.5 + 1,
+                Math.random() * 0.4,
+                Math.random() * 0.4,
+                Math.random() * 0.2 + 1,
+                (params.canvasWidth / params.codeLength) * i + params.fontSize/10,
+                params.canvasHeight - (params.canvasHeight - params.fontSize + 16 ) / 2);
+
+            if(typeof text == 'string'){
+                ctx.fillText(text.charAt(i), 0, 0)
+            }else if(typeof text == 'object'){
+                ctx.fillText(text[i], 0, 0)
+            }
+        }
+
+        return yield new Promise(function(resolve, reject) {
+            canvas.toBuffer(function(err, data) {
+                if (err) {
+                    return reject(err)
+                } else {
+                    return resolve({answer: answer, imageBuffer: data})
+                }
+            })
+        });
+    }
+}

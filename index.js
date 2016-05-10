92334f510a7cd0bce01ade0527ae107e79cf883d
yukrain <yukrain@163.com>
2016-05-10 16:50:04 +0800

    format code

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 var Canvas = require('canvas');
-var Font = Canvas.Font;
-var path = require("path");
-
+var Font   = Canvas.Font;
+var path   = require("path");
 if (!Font) {
     throw new Error('Need to compile with font support');
 }
 
-module.exports = function(params) {
-
-    params.color = params.color || 'rgb(0,0,0)'
-    params.background = params.background || 'rgb(255,255,255)'
-    params.lineWidth = params.lineWidth || 1
-    params.fontSize = params.fontSize || 30
-    params.codeLength = params.length || 4
-    params.canvasWidth = params.width || 150
+module.exports = function (params) {
+    params.color        = params.color || 'rgb(0,0,0)'
+    params.background   = params.background || 'rgb(255,255,255)'
+    params.lineWidth    = params.lineWidth || 1
+    params.fontSize     = params.fontSize || 30
+    params.codeLength   = params.length || 4
+    params.canvasWidth  = params.width || 150
     params.canvasHeight = params.height || 32
-    params.type = params.type || 'normal';     //normal 普通  number 仅数字 letter 仅字母 arithmetic 算数题
-    params.fontPath = params.fontPath ||  path.join(__dirname, './font/captchaFont.ttf');
+    params.type         = params.type || 'normal';     //normal 普通  number 仅数字 letter 仅字母 arithmetic 算数题
+    params.fontPath = params.fontPath || path.join(__dirname, './font/captchaFont.ttf');
 
     return function*() {
 
         var canvas = new Canvas(params.canvasWidth, params.canvasHeight)
-        var ctx = canvas.getContext('2d')
+        var ctx    = canvas.getContext('2d')
 
         ctx.antialias = 'gray'
         ctx.fillStyle = params.background
@@ -30,9 +28,9 @@ module.exports = function(params) {
         ctx.fillStyle = params.color
         ctx.lineWidth = params.lineWidth
 
-        var enFont = new Font('enFont',  params.fontPath);
+        var enFont = new Font('enFont', params.fontPath);
         ctx.addFont(enFont);
-        ctx.font = 'normal normal '+params.fontSize + 'px enFont'
+        ctx.font = 'normal normal ' + params.fontSize + 'px enFont'
 
         ctx.strokeStyle = 'grey'
         for (var i = 0; i < 3; i++) {
@@ -44,37 +42,37 @@ module.exports = function(params) {
         ctx.strokeStyle = params.color
         var text;
         var answer;
-        switch ( params.type ){
+        switch (params.type) {
 
             case 'number':
-                answer = text =  Math.random().toString(10).substr(2, params.codeLength).toLocaleUpperCase();
+                answer = text = Math.random().toString(10).substr(2, params.codeLength).toLocaleUpperCase();
                 break;
 
             case 'letter':
-                for(var i =0; i<params.codeLength; i++){
-                    text +=  String.fromCharCode(Math.ceil(Math.random() * 25) + 65).toLocaleUpperCase();
+                for (var i = 0; i < params.codeLength; i++) {
+                    text += String.fromCharCode(Math.ceil(Math.random() * 25) + 65).toLocaleUpperCase();
                 }
                 answer = text;
                 break;
 
             case 'arithmetic':
-                switch( Math.ceil(Math.random() * 4) ){
+                switch (Math.ceil(Math.random() * 4)) {
                     //加
                     case 1:
                         var num1 = Math.floor(Math.random() * 100);
                         var num2 = Math.floor(Math.random() * 100);
-                        text =  [num1, "+", num2, "="];
-                        answer = num1 + num2;
+                        text     = [num1, "+", num2, "="];
+                        answer   = num1 + num2;
                         break;
                     //减
                     case 2:
                         var num1 = Math.floor(Math.random() * 100);
                         var num2 = Math.floor(Math.random() * 100);
-                        if(num1 > num2){
-                            text =  [num1, "-", num2, "="];
+                        if (num1 > num2) {
+                            text   = [num1, "-", num2, "="];
                             answer = num1 - num2;
-                        }else{
-                            text =  [num2, "-", num1, "="];
+                        } else {
+                            text   = [num2, "-", num1, "="];
                             answer = num2 - num1;
                         }
                         break;
@@ -82,16 +80,16 @@ module.exports = function(params) {
                     case 3:
                         var num1 = Math.ceil(Math.random() * 20);
                         var num2 = Math.ceil(Math.random() * 9);
-                        text =  [num1, "×", num2, "="];
-                        answer = num1 * num2;
+                        text     = [num1, "×", num2, "="];
+                        answer   = num1 * num2;
 
                         break;
                     //除
                     case 4:
                         var num1 = Math.ceil(Math.random() * 9);
                         var num2 = Math.ceil(Math.random() * 8 + 1);
-                        text =  [ num1 * num2, "÷", num1, "="];
-                        answer = num2;
+                        text     = [num1 * num2, "÷", num1, "="];
+                        answer   = num2;
                         break;
                 }
 
@@ -99,14 +97,13 @@ module.exports = function(params) {
                 break;
 
             default:
-                answer =text =  Math.random().toString(35).substr(2, params.codeLength).toLocaleUpperCase();
+                answer = text = Math.random().toString(35).substr(2, params.codeLength).toLocaleUpperCase();
         }
 
         //自定义验证码
-        if(params.text){
+        if (params.text) {
             answer = text = params.text;
         }
-
 
         for (i = 0; i < text.length; i++) {
             ctx.setTransform(
@@ -114,18 +111,18 @@ module.exports = function(params) {
                 Math.random() * 0.4,
                 Math.random() * 0.4,
                 Math.random() * 0.2 + 1,
-                (params.canvasWidth / params.codeLength) * i + params.fontSize/10,
+                (params.canvasWidth / params.codeLength) * i + params.fontSize / 10,
                 params.canvasHeight - (params.canvasHeight - params.fontSize + 16 ) / 2);
 
-            if(typeof text == 'string'){
+            if (typeof text == 'string') {
                 ctx.fillText(text.charAt(i), 0, 0)
-            }else if(typeof text == 'object'){
+            } else if (typeof text == 'object') {
                 ctx.fillText(text[i], 0, 0)
             }
         }
 
-        return yield new Promise(function(resolve, reject) {
-            canvas.toBuffer(function(err, data) {
+        return yield new Promise(function (resolve, reject) {
+            canvas.toBuffer(function (err, data) {
                 if (err) {
                     return reject(err)
                 } else {
